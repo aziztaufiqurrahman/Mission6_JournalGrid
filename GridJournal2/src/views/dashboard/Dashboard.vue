f9d0f69e2dd238402e800a21d4b3daaf1e876586
nabiilahnadaa <111941049+nabiilahnadaa@users.noreply.github.com>
2022-11-19 18:34:34 +0700

    Add files via upload

---
+++ GridJournal2/src/views/dashboard/Dashboard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import Quotes from "./dashboardComponents/quotesBox/quotes.vue";
+import ProductMonthTable from "./dashboardComponents/project-month-table/ProductMonthTable.vue";
+import BlogCard from "./dashboardComponents/blog-card/BlogCard.vue";
+</script>
+
+<template>
+  
+  <v-row>
+    <v-col cols="12" sm="12">
+      <Quotes />
+    </v-col>
+    <v-col cols="12" lg="8" class="d-flex align-items-stretch">
+      <ProductMonthTable />
+    </v-col>
+    <v-col cols="12" sm="12">
+    <BlogCard />
+    </v-col>
+  </v-row>
+</template>
---
+++ GridJournal2/src/views/dashboard/dashboardComponents/journalCard/MyJournal.vue
@@ -0,0 +1,231 @@
+<template>
+    <v-card
+      class="mx-auto"
+      max-width="auto"
+    >
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        height="200px"
+      ></v-img>
+  
+      <v-card-title>
+        Top western road trips
+      </v-card-title>
+  
+      <v-card-subtitle>
+        1,000 miles of wonder
+      </v-card-subtitle>
+  
+      <v-card-actions>
+        <v-btn
+          color="orange lighten-2"
+          text
+          @click="show = !show"
+        >
+          Explore
+        </v-btn>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn
+          icon
+          @click="show = !show"
+        >
+          <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+  
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+  
+          <v-container class="bg-surface-variant">
+                    <v-row no-gutters>
+                      <v-col>
+                        <v-sheet class="pa-1 ma-1">
+                          <v-textarea
+                            class="greyy"
+                            color="cyan"
+                            auto-grow
+                            variant="outlined"
+                            rows="5"
+                            clearable
+                            label="How are you today"
+                          ></v-textarea>
+                        <v-spacer></v-spacer>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Close
+                            </v-btn>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Save
+                        </v-btn>
+                     </v-sheet>
+                      </v-col>
+                      <v-col>
+                        <v-sheet class="pa-1 ma-1">
+                            <v-textarea
+                            class="greyy"
+                            color="cyan"
+                            auto-grow
+                            variant="outlined"
+                            rows="5"
+                            clearable
+                            label="How are you today"
+                          ></v-textarea>
+                        <v-spacer></v-spacer>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Close
+                            </v-btn>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Save
+                        </v-btn>
+                        </v-sheet>
+                      </v-col>
+
+                      <v-col>
+                        <v-sheet class="pa-1 ma-1">
+                          <v-textarea
+                            class="greyy"
+                            color="cyan"
+                            auto-grow
+                            variant="outlined"
+                            rows="5"
+                            clearable
+                            label="How are you today"
+                          ></v-textarea>
+                        <v-spacer></v-spacer>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Close
+                            </v-btn>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Save
+                        </v-btn>
+                        </v-sheet>
+                      </v-col>
+
+                      <v-responsive width="100%"></v-responsive>
+
+                      <v-col>
+                        <v-sheet class="pa-1 ma-1">
+                            <v-textarea
+                            class="greyy"
+                            color="cyan"
+                            auto-grow
+                            variant="outlined"
+                            rows="5"
+                            clearable
+                            label="How are you today"
+                          ></v-textarea>
+                        <v-spacer></v-spacer>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Close
+                            </v-btn>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Save
+                        </v-btn>
+                        </v-sheet>
+                      </v-col>
+
+                      <v-col>
+                        <v-sheet class="pa-1 ma-1">
+                          <v-textarea
+                            class="greyy"
+                            color="cyan"
+                            auto-grow
+                            variant="outlined"
+                            rows="5"
+                            clearable
+                            label="How are you today"
+                          ></v-textarea>
+                        <v-spacer></v-spacer>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Close
+                            </v-btn>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Save
+                        </v-btn>
+                        </v-sheet>
+                      </v-col>
+
+                      <v-col>
+                        <v-sheet class="pa-1 ma-1">
+                          <v-textarea
+                            class="greyy"
+                            color="cyan"
+                            auto-grow
+                            variant="outlined"
+                            rows="5"
+                            clearable
+                            label="How are you today"
+                          ></v-textarea>
+                        <v-spacer></v-spacer>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Close
+                            </v-btn>
+                            <v-btn
+                                color="white"
+                                text
+                                @click="dialog = false"
+                            >
+                                Save
+                        </v-btn>
+                        </v-sheet>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </template>
+
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+}
+</script>
